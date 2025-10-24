43552dac218128fdcaf99a35aa03c2ea2d6dd7bb
FedeO <fede.osorio@outlook.com.ar>
2025-10-23 21:53:25 -0300

    Se agrega estructura para creación de usuarios

--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,35 +1,83 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './SignUp.css';
+import userService from '../../service/userService';
+import * as React from 'react';
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    password: '',
+  });
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(`SignUp mounted`)
-  }, [])
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formData.email !== confirmEmail) {
+      alert("Los correos electrónicos no coinciden.");
+      return;
+    }
+
+    if (formData.password !== confirmPassword) {
+      alert("Las contraseñas no coinciden.");
+      return;
+    }
+    
+    const userData = {
+      email: formData.email,
+      username: formData.username,
+      password: formData.password
+    };
+
+    try {
+      await userService.registerUser(userData);
+      alert('¡Registro exitoso!');
+      navigate('/login');
+    } catch (error) {
+      console.error("Error en el registro:", error);
+      alert("Hubo un error durante el registro.");
+    }
+  };
 
   return (
     <>
       <div className="SignUp-component">
         <div className='formContainer'>
           <div className='logoSign'>
-            <img src='/img/logo.png'></img>
+            <img src='/img/logo.png' alt="Logo"></img>
             <h4>La Esquina Literaria</h4>
           </div>
-          <form className='formSign'>
-            <label htmlFor="name">Email:</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="email">Confirmar Email:</label>
+          <form className='formSign' onSubmit={handleSubmit}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+
+            <label htmlFor="emailB">Confirmar Email:</label>
             <span className='signInfo'>Ingrese nuevamente su correo electrónico.</span>
-            <input type="email" id="emailB" name="emailB" />
+            <input type="email" id="emailB" name="emailB" value={confirmEmail} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirmEmail(e.target.value)} required />
+            
             <label htmlFor="username">Usuario:</label>
             <span className='signInfo'>Solo letras y números.</span>
-            <input type="text" id="username" name="username" />
+            <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} required />
+            
             <label htmlFor="password">Contraseña:</label>
             <span className='signInfo'>Mínimo 6 caracteres.</span>
-            <input type="password" id="password" name="password" autoComplete="new-password"  />
+            <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} autoComplete="new-password" required />
+            
             <label htmlFor="confirm-password">Confirmar Contraseña:</label>
-            <input type="password" id="confirm-password" name="confirm-password" autoComplete="new-password" />
+            <input type="password" id="confirm-password" name="confirm-password" value={confirmPassword} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)} autoComplete="new-password" required />
+            
             <button className='buttonSign' type="submit">Registrarse</button>
             <Link to="/login" className="login-link">¿Ya estás registrado? Inicia sesión.</Link>
           </form>
---
+++ frontend/src/service/userService.js
@@ -0,0 +1,25 @@
+const SERVER_URL = "http://localhost:5000";
+
+const userService = {
+    registerUser: async (userData) => {
+        console.log(userData);
+
+        const response = await fetch(`${SERVER_URL}/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        if (!response.ok) {
+            console.log(response);
+            const errorData = await response.json();
+            throw new Error(errorData.message || "Error al registrar el usuario.");
+        }
+
+        return response.json();
+    },
+}
+
+export default userService;
