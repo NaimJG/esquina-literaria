93f655b9fa77f0ce4a4ba67a607b84cca51739b2
FedeO <fede.osorio@outlook.com.ar>
2025-10-27 22:01:23 -0300

    Se agrega icono de carrito y foto de avatar al perfil

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,42 @@
-// import React from 'react'
+import React from "react";
 import "./Header.css";
 import { Link, NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/useAuth';
+import Avatar from "@mui/material/Avatar";
+import { Tooltip } from "@mui/material";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
   const { user } = useAuth();
+
+  function stringToColor(string: string) {
+    let hash = 0;
+    let i;
+
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+
+    return color;
+  }
+
+  function stringAvatar(name: string) {
+    const nameParts = name.split(' ');
+    const initials = (nameParts[0]?.[0] || '') + (nameParts[1]?.[0] || '');
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: initials.toUpperCase(),
+    };
+  }
 
   return (
     <>
@@ -22,8 +54,19 @@ function Header() {
             <div className="authButtons">
               {user ? (
                 <>
-                  <ul className='navList'>
-                    <li><NavLink to="/profile" className={({ isActive }) => isActive ? 'nav-active' : 'nav-li'}>Perfil</NavLink></li>
+                  <ul className='navList navCart' style={{ alignItems: 'center' }}>
+                    <li className="cartContainer">
+                      <Tooltip title="Carrito">
+                        <ShoppingCartIcon></ShoppingCartIcon>
+                      </Tooltip>
+                    </li>
+                    <li>
+                      <Tooltip title="Ver Perfil">
+                        <NavLink to="/profile">
+                          <Avatar {...stringAvatar(user.username)} />
+                        </NavLink>
+                      </Tooltip>
+                    </li>
                   </ul>
                 </>
               ) : (
